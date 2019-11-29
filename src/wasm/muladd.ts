0b18485d00303505e1fa088bf6d829e92791c523
Orestis Floros <forestis@kth.se>
2019-11-29 10:24:44 +0100

    muladd.ts: Remove useless | 0
    Not needed in static-typed AssemblyScript

--- src/wasm/muladd.ts
+++ src/wasm/muladd.ts
@@ -1,4 +1,3 @@
-// TODO: delete | 0
 export function muladd_loop(xlen: i32, start: i32, end: i32, Y: i32, i: i32, c: i32): i32 {
     var hy = (Y >>> 14);
     var ly = (Y & 0x3fff);
@@ -10,11 +9,9 @@ export function muladd_loop(xlen: i32, start: i32, end: i32, Y: i32, i: i32, c: 
 
         var hx = (x_j >>> 14);
         var lx = (x_j & 0x3fff);
-        var cross = (hx * ly + lx * hy) | 0;
-        lx = (((w_ji | 0) + lx * ly +
-            ((cross & 0x3fff) << 14)) | 0) + c;
-        c = ((lx >>> 28) + hx * hy +
-            (cross >>> 14)) | 0;
+        var cross = hx * ly + lx * hy;
+        lx = w_ji + lx * ly + ((cross & 0x3fff) << 14) + c;
+        c = (lx >>> 28) + hx * hy + (cross >>> 14);
 
         store<i32>((j + i + xlen) << 2, lx & 0xfffffff);  // w[j + i]
     }
