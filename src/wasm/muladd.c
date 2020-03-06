16b71ecb037786faf6851f13a310a8ac02f2562e
Orestis Floros <forestis@kth.se>
2020-03-06 17:29:59 +0100

    tmp

--- src/wasm/muladd.c
+++ src/wasm/muladd.c
@@ -1,28 +1,19 @@
 #define SIZE 8989
 int buffer[SIZE];
 
-int tmp(int len, int start, int value) {
-    int result = 0;
-    for (int i = start; i < len; i ++ ){
-        result+=buffer[i];
-        buffer[i] = value;
-    }
-    return result;
-}
-
 int muladd_loop(int xlen, int start, int end, int Y, int i, int c) {
-    int hy = (Y >> 14);  // XXX: was >>>
+    int hy = (Y >> 14);
     int ly = (Y & 0x3fff);
 
     for (int j = start; j < end; j++) {
         int x_j = buffer[j];
         int w_ji = buffer[j + i + xlen];
 
-        int hx = (x_j >> 14);  // XXX: was >>>
+        int hx = (x_j >> 14);
         int lx = (x_j & 0x3fff);
         int cross = hx * ly + lx * hy;
         lx = w_ji + lx * ly + ((cross & 0x3fff) << 14) + c;
-        c = (lx >> 28) + hx * hy + (cross >> 14);  // XXX: both were >>>
+        c = (lx >> 28) + hx * hy + (cross >> 14);
 
         buffer[j + i + xlen] = lx & 0xfffffff;
     }
